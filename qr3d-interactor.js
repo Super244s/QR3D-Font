71099e82fc8c4516a68e8da1cd8f7dc220e01296
Super244s <Phupingbut244@gmail.com>
2026-07-17 12:35:12 +0700

    Add enable function to qr3d-interactor module

--- qr3d-interactor.js
+++ qr3d-interactor.js
@@ -1,1 +1,11 @@
+module.exports = {
+  enable(data) {
+    return {
+      canOpenLink: !!data.link,
+      canFillForm: !!data.form,
+      canSubmit: true,
+      extendedSupport: "พร้อมเพิ่มฟังก์ชันในอนาคต"
+    };
+  }
+};
 
